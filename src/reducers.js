4777654f8d24b755b0983a00daf30a927103e166
Rohit-del-source <rohitverma18158920@gmail.com>
2024-01-21 18:44:41 +0530

    donee

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,38 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+    const ADD_USER = 'ADD_USER';
+    const FILL_FUEL = 'FILL_FUEL';
+const reducer = (state, action) => {
+    switch(action.type){
+        case ADD_USER:
+            return{
+                ...state,
+                users: [
+                    ...state.users,
+                    {
+                        id: action.id,
+                        name: action.name,
+                    },
+                ],
+            };
+
+        case FILL_FUEL:
+            return state; 
+
+        default:
+            return state;
+
+    }    
+};
+
+export const addUserAction = (id, name) => ({
+    type: ADD_USER,
+    id,
+    name,
+});
+
+export const fillFuelAction = () =>({
+    type: FILL_FUEL,
+});
 
 export default reducer;
